3f5c2172ed0796a96febd5ade6753f1e77c2ab59
MasonSchnell <91274207+MasonSchnell@users.noreply.github.com>
2023-10-07 23:23:52 -0400

    Added the post starter code

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const notes = require("./Develop/db/db.json");
 const fs = require("fs");
+const bodyParser = require("body-parser");
 // const path = require("path");
 const app = express();
+
+app.use(bodyParser.json());
 
 // app.use(express.static("assets"));
 
@@ -12,14 +15,16 @@ app.get("/api/notes", (clientRequestObj, serverResponseObj) => {
     serverResponseObj.json(notes);
 });
 
-app.get("/api/notes/:index", (clientRequestObj, serverResponseObj) => {
-    const index = clientRequestObj.params.index;
-    console.log(index);
-    const obj = notes.find((funObj) => funObj.id === index);
-    console.log(obj);
-    serverResponseObj.send(obj);
-});
+// Handles mak
+// app.get("/api/notes/:index", (clientRequestObj, serverResponseObj) => {
+//     const index = clientRequestObj.params.index;
+//     console.log(index);
+//     const obj = notes.find((funObj) => funObj.id === index);
+//     console.log(obj);
+//     serverResponseObj.send(obj);
+// });
 
+// Handles deleting the displayed notes
 app.delete("/api/notes/:index", (clientRequestObj, serverResponseObj) => {
     const index = clientRequestObj.params.index;
     const obj = notes.find((funObj) => funObj.id === index);
@@ -29,6 +34,26 @@ app.delete("/api/notes/:index", (clientRequestObj, serverResponseObj) => {
     const jNotes = JSON.stringify(notes);
 
     fs.writeFile("./Develop/db/db.json", jNotes, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+        } else {
+            console.log("Data updated and saved to file.");
+        }
+    });
+});
+
+app.get("/api/notes", (clientRequestObj, serverResponseObj) => {
+    serverResponseObj.json(notes);
+});
+
+app.post("/api/notes", (clientRequestObj, serverResponseObj) => {
+    const note = clientRequestObj.body;
+    console.log(note);
+
+    notes.push(note);
+    const newNotes = JSON.stringify(notes);
+
+    fs.writeFile("./Develop/db/db.json", newNotes, (err) => {
         if (err) {
             console.error("Error writing to file:", err);
         } else {
